cbe01b48ddc9050bd94d4ed924c1287d0acef7bf
rkrovvidi <rkrovvidi@example.com>
2019-03-24 18:19:42 -0700

    ArrayBuffer

---
+++ src/ArrayBuffer_Concept.scala
@@ -0,0 +1,33 @@
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Created by rkrovvidi on 3/24/19.
+  */
+object ArrayBuffer_Concept extends App{
+
+
+  // general-purpose collections class for mutable, indexed sequential collections.
+//ArrayBuffer is an indexed sequential collection. Use ListBuffer if you prefer a linear sequential collection that is mutable.
+
+
+
+  var fruits = ArrayBuffer[String]()
+  var ints = ArrayBuffer[Int]()
+  var people = ArrayBuffer[Person]()
+
+  // ready to hold 100,000 ints
+  val buf = new ArrayBuffer[Int](100000)
+
+
+
+  val nums = ArrayBuffer(1, 2, 3)
+
+//  val people_1 = ArrayBuffer(
+//    Person("Al"),
+//    Person("Kim"),
+//    Person("Jim")
+//  )
+
+  class Person(var name: String){}
+
+}
