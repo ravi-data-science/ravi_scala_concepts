9d37db20011ebc7f47c0a5c47988b64cee9bcba2
rkrovvidi <rkrovvidi@example.com>
2019-03-25 01:54:52 -0700

    -

---
+++ src/For_Loop_Over_Map.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by rkrovvidi on 3/25/19.
+  */
+object For_Loop_Over_Map extends App{
+
+
+  val names = Map("fname" -> "Robert", "lname" -> "Goren")
+
+  for ((k,v) <- names) println(s"key: $k, value: $v")
+
+}
