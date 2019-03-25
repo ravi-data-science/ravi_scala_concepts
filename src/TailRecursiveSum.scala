83fe9015b03d0219e32df22e2f4bf271974706c6
rkrovvidi <rkrovvidi@example.com>
2019-03-25 09:56:51 -0700

    -

---
+++ src/TailRecursiveSum.scala
@@ -0,0 +1,28 @@
+import scala.annotation.tailrec
+
+/**
+  * Created by rkrovvidi on 3/10/19.
+  */
+//tail recursion
+object TailRecursiveSum extends App {
+
+  // call sum
+  println(sum(List.range(1, 10)))
+
+  // the tail-recursive version of sum
+  def sum(list: List[Int]): Int = {
+    @tailrec
+    def sumWithAccumulator(list: List[Int], currentSum: Int): Int = {
+      list match {
+        case Nil => {
+          val stackTraceAsArray = Thread.currentThread.getStackTrace
+          stackTraceAsArray.foreach(println)
+          currentSum
+        }
+        case x :: xs => sumWithAccumulator(xs, currentSum + x)
+      }
+    }
+    sumWithAccumulator(list, 0)
+  }
+
+}
