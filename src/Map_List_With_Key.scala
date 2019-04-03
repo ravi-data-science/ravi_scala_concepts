54abe31bdcf500d207c835decc9fd77fd7e7b47b
rkrovvidi <rkrovvidi@example.com>
2019-04-03 14:59:29 -0700

    -

---
+++ src/Map_List_With_Key.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by rkrovvidi on 4/3/19.
+  */
+object Map_List_With_Key {
+
+  def main(args: Array[String]): Unit = {
+
+
+    val mapListWithKey = Map({"andhra,tamilnadu"} -> "india")
+
+    println(mapListWithKey)
+  }
+
+}
