1cdd796cc4a06c04c885034cd68d9b7bb2d638d9
rkrovvidi <rkrovvidi@example.com>
2019-04-03 16:13:23 -0700

    -

---
+++ src/Array_Sum.scala
@@ -0,0 +1,36 @@
+/**
+  * Created by rkrovvidi on 4/3/19.
+  */
+object Array_Sum extends App{
+
+
+  val arr = Array(1,2,4,5,7)
+
+  var sum1 =0
+  val arr_sum = for(ctr <- arr)
+  {
+
+    println(ctr)
+
+    sum1 = sum1 + ctr
+
+  }
+
+  println(sum1)
+
+  println(arr_sum)
+
+  val arr_sum_1 = for(ctr <- arr)
+    yield
+  {
+
+    println(ctr)
+
+    ctr
+
+  }
+
+  println(arr_sum_1.toList)
+
+
+}
